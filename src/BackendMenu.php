93b10f0ea174619aa55406fd1b60f01862c6cb4e
Jan Chuchvalec <chuchy4ever@gmail.com>
2023-12-29 18:02:19 +0100

    test

---
+++ src/BackendMenu.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chuchy4ever\TaxonomyEditor;
+
+use Bolt\Menu\ExtensionBackendMenuInterface;
+use Knp\Menu\MenuItem;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+final class BackendMenu implements ExtensionBackendMenuInterface
+{
+
+    private UrlGeneratorInterface $urlGenerator;
+
+    private TranslatorInterface $translator;
+
+    public function __construct(
+        UrlGeneratorInterface $urlGenerator,
+        TranslatorInterface $translator
+    )
+    {
+        $this->urlGenerator = $urlGenerator;
+        $this->translator = $translator;
+    }
+
+    public function addItems(MenuItem $menu): void
+	{
+        $menu->addChild('Taxonomy', [
+            'extras' => [
+                'name' => $this->translator->trans('caption.taxonomy'),
+                'type' => 'separator',
+                'icon' => 'fa-wrench',
+            ],
+        ]);
+        $menu->addChild('File Management2', [
+            'uri' => $this->urlGenerator->generate('bolt_menupage', [
+                'slug' => 'filemanagement',
+            ]),
+            'extras' => [
+                'name' => $this->translator->trans('caption.file_management'),
+                'icon' => 'fa-folder-open',
+                'slug' => 'filemanagement2',
+            ],
+        ]);
+	}
+}
