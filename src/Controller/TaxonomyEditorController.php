a9745d240f736458c09b7738b8419f2e1a032e7b
Jan Chuchvalec <chuchy4ever@gmail.com>
2023-12-30 15:00:52 +0100

    test

--- src/Controller/TaxonomyEditorController.php
+++ src/Controller/TaxonomyEditorController.php
@@ -9,16 +9,19 @@ use Bolt\Controller\TwigAwareController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/taxonomy-editor")
+ */
 final class TaxonomyEditorController extends TwigAwareController implements BackendZoneInterface
 {
-    /**
-     * @Route("/taxonomy-editor/", name="taxonomy_editor_list")
-     */
-    public function list(): Response
-    {
-        return $this->render('taxonomy_editor_list.html.twig', [
-            'title' => 'User content'
-        ]);
-    }
+	/**
+	 * @Route(name="taxonomy_editor_list", methods={'GET'})
+	 */
+	public function list(): Response
+	{
+		return $this->render('taxonomy_editor_list.html.twig', [
+		 'title' => 'User content'
+		]);
+	}
 
 }
