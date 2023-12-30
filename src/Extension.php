ff1e71ce2e1c883c6a052b59994340ae13ca725c
Jan Chuchvalec <chuchy4ever@gmail.com>
2023-12-30 18:30:56 +0100

    configy

--- src/Extension.php
+++ src/Extension.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Chuchy4ever\TaxonomyEditor;
 
 use Bolt\Extension\BaseExtension;
+use Symfony\Component\Filesystem\Filesystem;
 
 final class Extension extends BaseExtension
 {
@@ -17,4 +18,16 @@ final class Extension extends BaseExtension
     {
         $this->addTwigNamespace('taxonomy-editor');
     }
+
+	public function install(): void
+	{
+		$projectDir = $this->getContainer()->getParameter('kernel.project_dir');
+		$public = $this->getContainer()->getParameter('bolt.public_folder');
+
+		$source = dirname(__DIR__) . '/assets/';
+		$destination = $projectDir . '/' . $public . '/assets/';
+
+		$filesystem = new Filesystem();
+		$filesystem->mirror($source, $destination);
+	}
 }
